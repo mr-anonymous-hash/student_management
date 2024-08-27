1935884069a25ddee9a51ace0f28b8ca4a2fb615
mr-anonymous-hash <sibivarma11@gmail.com>
2024-08-27 20:47:51 +0530

    route path change

--- student_management_backend/src/routes/attendance_route.js
+++ student_management_backend/src/routes/attendance_route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const attendance_controller = require('./../controllers/attendance_controller')
 
 router.get('/',attendance_controller.getAllAttendance)
-router.get('/:id',attendance_controller.getAttendanceById)
+router.get('/:class_name',attendance_controller.getAttendanceByClass)
 router.post('/',attendance_controller.createAttendance)
 router.put('/:id',attendance_controller.updateAttendance)
 router.delete('/:id',attendance_controller.deleteAttendance)
