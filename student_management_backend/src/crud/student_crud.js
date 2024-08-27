464a59b774b99743a76644610ef5ffac77381e16
mr-anonymous-hash <sibivarma11@gmail.com>
2024-08-27 21:30:23 +0530

    method name change

--- student_management_backend/src/crud/student_crud.js
+++ student_management_backend/src/crud/student_crud.js
@@ -3,10 +3,10 @@ const db_factory = require('./../utils/db_factory')
 
 const getAllStudents = async() => await db_factory.getAllRecords(Students)
 
-const getStudentByClass = async(class_name) => await db_factory.getRecordById(Students, class_name)
+const getStudentsByClass = async(class_name) => await db_factory.getRecordById(Students, class_name)
 
 const createStudent = async(data) => await db_factory.createRecord(Students, data)
 
 module.exports = {
-    getAllStudents,getStudentByClass,createStudent
+    getAllStudents,getStudentsByClass,createStudent
 }
