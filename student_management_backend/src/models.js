005b50a53c0911d062270cb003bd21d68213af36
mr-anonymous-hash <sibivarma11@gmail.com>
2024-08-29 09:25:35 +0530

    add associations to models

--- student_management_backend/src/models.js
+++ student_management_backend/src/models.js
@@ -1,56 +1,81 @@
 const sequelize = require('./database');
-const DataTypes = require('sequelize');
+const { DataTypes } = require('sequelize');
 
+// Classroom Model
 const Classroom = sequelize.define('classrooms', {
-    class_name:{
-        type: DataTypes.STRING,
-        allowNull:false,
-        unquie: true
-    }
-  });
-  
-const Students = sequelize.define('students',{
-    student_name:{
-        type: DataTypes.STRING,
-        allowNull:false
-    },
-    roll_no:{
-        type: DataTypes.STRING,
-        allowNull:false
-    },
-    class_name:{
-        type: DataTypes.STRING,
-        allowNull:false
-    }
-})
-
-const Attendance = sequelize.define('attendance',{
-    attendance_date:{
-        type: DataTypes.DATE,
-        allowNull:false
-    },
-    roll_no:{
-        type: DataTypes.STRING,
-        allowNull:false
-    },
-    class_name:{
-        type: DataTypes.STRING,
-        allowNull:false
-    },
-    attendance_status:
-    {
-        type: DataTypes.BOOLEAN,
-        allowNull:false
-    }
-
-
-})
-  // Sync the database and model
-  sequelize.sync().then(() => {
-    console.log('Database & tables created!');
-  });
-  
-
-  module.exports = {
-    Classroom,Students,Attendance
+  class_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true // Corrected the spelling of 'unique'
   }
+});
+
+// Students Model
+const Students = sequelize.define('students', {
+  student_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  roll_no: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  class_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+// Attendance Model
+const Attendance = sequelize.define('attendance', {
+  attendance_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  roll_no: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  class_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  attendance_status: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  }
+});
+
+// Associations
+Classroom.hasMany(Students, {
+  foreignKey: 'class_name',
+  sourceKey: 'class_name',
+  as: 'students'
+});
+
+Students.belongsTo(Classroom, {
+  foreignKey: 'class_name',
+  targetKey: 'class_name',
+  as: 'classroom'
+});
+
+Students.hasMany(Attendance, {
+  foreignKey: 'roll_no',
+  sourceKey: 'roll_no',
+  as: 'attendances'
+});
+
+Attendance.belongsTo(Students, {
+  foreignKey: 'roll_no',
+  targetKey: 'roll_no',
+  as: 'student'
+});
+
+// Sync the database and model
+sequelize.sync().then(() => {
+  console.log('Database & tables created!');
+});
+
+module.exports = {
+  Classroom, Students, Attendance
+};
